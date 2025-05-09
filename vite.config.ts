0ee80fb7119a02fc0370093446368db574961e06
whbbit1999 <luckyboy_dong@sina.com>
2025-05-09 18:19:10 +0800

    build: remove debugger and console.log when building

--- vite.config.ts
+++ vite.config.ts
@@ -61,4 +61,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  esbuild: {
+    drop: ['debugger'],
+    pure: ['console.log'],
+  },
 })
