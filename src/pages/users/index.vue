9359b35ed746204c014f8aa655ab852d5a992d1e
Whbbit1999 <60510247+Whbbit1999@users.noreply.github.com>
2025-07-29 17:56:46 +0800

    feat(data-table): Now we can use this component to quickly generate a… (#21)

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Loader } from 'lucide-vue-next'
+
 import Page from '@/components/global-layout/basic-page.vue'
 
 import { columns } from './components/columns'
@@ -6,6 +8,15 @@ import DataTable from './components/data-table.vue'
 import UserCreate from './components/user-create.vue'
 import UserInvite from './components/user-invite.vue'
 import { users } from './data/users'
+
+const loading = ref(false)
+
+function mockLoading() {
+  loading.value = true
+  setTimeout(() => {
+    loading.value = false
+  }, 2000)
+}
 </script>
 
 <template>
@@ -17,9 +28,12 @@ import { users } from './data/users'
     <template #actions>
       <UserInvite />
       <UserCreate />
+      <UiButton variant="outline" @click="mockLoading">
+        <Loader />Mock Loading
+      </UiButton>
     </template>
     <div class="overflow-x-auto">
-      <DataTable :data="users" :columns="columns" />
+      <DataTable :loading :data="users" :columns="columns" />
     </div>
   </Page>
 </template>
