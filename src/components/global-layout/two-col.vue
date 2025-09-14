112cb495f7d52a877a795a7c30d42f18034f673a
whbbit1999 <luckyboy_dong@sina.com>
2025-09-15 06:39:49 +0800

    feat: add TwoLayout in global layouts, example in settings/components/settings-layout #28

---
+++ src/components/global-layout/two-col.vue
@@ -0,0 +1,19 @@
+<script lang='ts' setup>
+import { cn } from '@/lib/utils'
+</script>
+
+<template>
+  <main
+    :class="cn(
+      `grid grid-cols-1 lg:grid-cols-[200px_1fr] gap-4 w-full`,
+    )"
+  >
+    <aside>
+      <slot name="aside" />
+    </aside>
+
+    <section>
+      <slot name="default" />
+    </section>
+  </main>
+</template>
