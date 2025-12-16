07708da2be6b8cd5b747d6cd0365fe32dd9783a5
whbbit1999 <luckyboy_dong@sina.com>
2025-12-16 14:21:47 +0800

    refactor: refactor layout components and improve sidebar navigation

--- src/components/global-layout/types.ts
+++ src/components/global-layout/types.ts
@@ -1,5 +1,13 @@
+import type { Component } from 'vue'
+
 export interface LayoutHeaderProps {
   title: string
   description: string
   sticky?: boolean
+}
+
+export interface TowColAsideNavItem {
+  title: string
+  url: string
+  icon?: Component
 }
