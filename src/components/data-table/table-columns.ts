1f853e9b48e257a92e3bcbe9301c4a43b7ad5448
whbbit1999 <luckyboy_dong@sina.com>
2025-12-16 15:42:21 +0800

    feat: add a radio button column to the data table

--- src/components/data-table/table-columns.ts
+++ src/components/data-table/table-columns.ts
@@ -4,6 +4,8 @@ import { h } from 'vue'
 
 import Checkbox from '@/components/ui/checkbox/Checkbox.vue'
 
+import RadioCell from './radio-cell.vue'
+
 export const SelectColumn: ColumnDef<any> = {
   id: 'select',
   header: ({ table }) => h(Checkbox, {
@@ -15,6 +17,23 @@ export const SelectColumn: ColumnDef<any> = {
     'modelValue': row.getIsSelected(),
     'onUpdate:modelValue': value => row.toggleSelected(!!value),
     'ariaLabel': 'Select row',
+  }),
+  enableSorting: false,
+  enableHiding: false,
+}
+
+export const RadioSelectColumn: ColumnDef<any> = {
+  id: 'radio-select',
+  header: () => null,
+  cell: ({ row, table }) => h(RadioCell, {
+    checked: row.getIsSelected(),
+    onClick: (event: MouseEvent) => {
+      event.stopPropagation()
+      // 取消所有行的选择
+      table.toggleAllRowsSelected(false)
+      // 选择当前行
+      row.toggleSelected(true)
+    },
   }),
   enableSorting: false,
   enableHiding: false,
